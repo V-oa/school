279c867e6dfb3af82793a50744cf58e719b44b94
V-oa <1259907705@qq.com>
2026-05-21 22:28:21 +0800

    Add files via upload

--- rag_engine.py
+++ rag_engine.py
@@ -63,7 +63,7 @@ def init_qa_chain():
         _global_retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
     
     
-    _global_llm_strict = ChatOpenAI(model="deepseek-chat", temperature=0.1,max_tokens=500)
+    _global_llm_strict = ChatOpenAI(model="deepseek-chat", temperature=0.1,max_tokens=100)
     
     strict_system_prompt = (
         "你是一个高校智能助理。请严格根据以下学校官方文件的内容回答学生的问题。\n"
@@ -76,10 +76,10 @@ def init_qa_chain():
     ])
     
   
-    _global_llm_casual = ChatOpenAI(model="deepseek-chat", temperature=0.7,max_tokens=500)
+    _global_llm_casual = ChatOpenAI(model="deepseek-chat", temperature=0.7,max_tokens=100)
     casual_system_prompt = (
-        "你是一个由 DeepSeek 驱动的全能超级人工智能助手，现在化身为学生们的伙伴，你的回答沉稳认真，有温度并且幽默风趣。\n"
-        "当前用户在与你进行日常沟通，你可以帮他们解答日常常识、写代码、日常吐槽等。\n"
+        "你是一个由 DeepSeek 驱动的高校智能助理，你的名字叫做ovo，现在化身为学生们的伙伴，你的回答温暖并且沉稳认真，很少有废话。\n"
+        "当前用户在与你进行日常沟通，你可以帮他们解答日常常识、解决生活问题、写代码、日常吐槽等。\n"
         "此时【完全脱离学校手册的限制】，你可以调用你作为通用人工智能的一切智慧直接回答！不要说‘抱歉未查到’这种死板的话。"
     )
     _global_casual_prompt = ChatPromptTemplate.from_messages([
